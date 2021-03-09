62911b8504e740a0d5bb51689a56f3ac3be64214
ArvinRad <radanarvin@gmail.com>
2021-03-09 04:54:58 +0000

    middleTest.js with Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,31 @@
-const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-assertArraysEqual(middle([5, 6, 7, 10]), [6, 7]);
-assertArraysEqual(middle(["Hello", "Lighthouse", "Labs"]), ["Lighthouse"]);
-assertArraysEqual(middle([5]), []);
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [] for [5]", () => {
+    assert.deepEqual(middle([5]), []);
+  });
+  it("returns [] for [5, 8]", () => {
+    assert.deepEqual(middle([5, 8]), []); 
+  });
+  it("returns ['Lighthouse'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(middle(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse']); 
+  });
+  it("returns [6, 9] for [5, 6, 9, 12]", () => {
+    assert.deepEqual(middle([5, 6, 9, 12]), [6, 9]);
+  });
+});
+
+
+
+
+
+
+// const middle = require('../middle');
+// const assertArraysEqual = require('../assertArraysEqual');
+// assertArraysEqual(middle([5, 6, 7, 10]), [6, 7]);
+// assertArraysEqual(middle(["Hello", "Lighthouse", "Labs"]), ["Lighthouse"]);
+// assertArraysEqual(middle([5]), []);
