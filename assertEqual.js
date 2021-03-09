215b9e224f3bfe9d6ed255d7a184431308488a48
ArvinRad <radanarvin@gmail.com>
2021-03-09 00:06:10 +0000

    Seperating main and test files- Head.js and assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,7 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected ) {
-    return console.log("Assertion Passed: " + actual + " === " + expected);
+    return console.log(`Assertion Passed: ${actual} === ${expected}`);
   } 
-  else return console.log("Assertion Failed: " + actual + " !== " + expected);
-}
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+  else return console.log(`Assertion Failed: ${actual} == ${expected}`);
+};
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,12 +1,5 @@
+const assertEqual = require('./assertEqual');
 const head = function(array) {
   return array[0];
 };
-const assertEqual = function(actual, expected) {
-  if (actual === expected ) {
-    return console.log(`Head: ${actual}`);
-  } 
-  else return console.log("Undefined");
-};
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+module.exports = head;
