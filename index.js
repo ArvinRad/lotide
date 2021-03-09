9693ecd5507f104a7e9df5652e3a249e986a8178
ArvinRad <radanarvin@gmail.com>
2021-03-09 05:16:39 +0000

    Collecting all utility functions in index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const eqObjects = require('./eqObjects');
+const findKeyByValue = require('./findKeyByValue');
+const map = require('./map');
+const letterPositions = require('./letterPositions');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findKey: findKey,
+  findKeyByVakue: findKeyByValue,
+  map: map,
+  letterPositions: letterPositions,
+  takeUntil: takeUntil,
+  without: without
+};
