6c5aa131605a05edf87e81e922fd99abe8433442
ArvinRad <radanarvin@gmail.com>
2021-03-30 00:30:33 +0000

    Refractoring previous version

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,10 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected ) {
-    return console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } 
-  else return console.log(`Assertion Failed: ${actual} == ${expected}`);
+  let myMessage = "";
+  if (actual === expected) {
+    myMessage = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    myMessage = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+  return console.log(myMessage);
 };
 module.exports = assertEqual;
