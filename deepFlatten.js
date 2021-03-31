6dfb9976ec8d472b34cdabf612acfde0e33442f6
ArvinRad <radanarvin@gmail.com>
2021-03-31 21:17:24 +0000

    Refactored

--- deepFlatten.js
+++ deepFlatten.js
@@ -1,20 +1,13 @@
 const deepFlatten = function(array) {
   let newArray = [];
-
-  const addItem = function(newArray, item) {
-    if (Array.isArray(item)) {
-      item.forEach(subItem => {
-        addItem(newArray,subItem);
-      });
-    } else {
-      newArray.push(item);
-    };
-    return newArray;
-  };
-
   array.forEach(item => {
-    addItem(newArray,item);
+    if (Array.isArray(item)) {
+    newArray.push(...deepFlatten(item));
+    } else {
+    newArray.push(item);
+    }
   });
   return newArray;
-}
+};
+
 module.exports = deepFlatten;
