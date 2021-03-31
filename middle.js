f719ed1519943c40ea7d6f69a2316fecb1788cde
ArvinRad <radanarvin@gmail.com>
2021-03-31 21:59:40 +0000

    Middle with Assertions

--- middle.js
+++ middle.js
@@ -5,8 +5,17 @@ const middle = function(array) {
   if (k > 2) {
     if (k %2 == 1 ) {
      result.push(array[(k - 1) / 2]);
-    } else result = array.slice((k / 2) - 1, (k / 2) + 1);
+    } else {
+    result = array.slice((k / 2) - 1, (k / 2) + 1);
     }
+  }
 return result;
 }
 module.exports = middle;
+
+const assertArraysEqual = require('./assertArraysEqual')
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
