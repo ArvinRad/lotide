3d84dfc9e97d77983fed24300f652920ddc5aac3
ArvinRad <radanarvin@gmail.com>
2021-03-31 21:45:20 +0000

    Flatten an array using Reduce

---
+++ flatten.js
@@ -0,0 +1,13 @@
+
+const flatten = function (arr) {
+  const output = arr.reduce((acc, item) => {
+    if (Array.isArray(item)) {
+      return acc.concat(...flatten(item));
+    } else {
+      return acc.concat(item);
+    }
+  }, []);
+  return output;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
