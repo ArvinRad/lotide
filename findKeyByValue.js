412a23b0a532a4139caac2d61733025eb1913d95
ArvinRad <radanarvin@gmail.com>
2021-04-01 03:11:17 +0000

    Refactored

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -9,13 +9,6 @@ const findKeyByValue = function(myObject, value) {
 };
 
 
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected ) {
-//     return console.log(`Assertion Passed: ${actual} === ${expected}`);
-//   }
-//   else return console.log(`Assertion Failed: ${actual} == ${expected}`);
-// };
-
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
