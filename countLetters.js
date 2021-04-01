f693faf97e95d65589d9d6db392d0a448af9a1e8
ArvinRad <radanarvin@gmail.com>
2021-04-01 02:21:47 +0000

    Refactored

--- countLetters.js
+++ countLetters.js
@@ -1,20 +1,22 @@
 const countLetters = function(myString) {
-result = {};
-  for (letters of myString) {
-    if (Number.isInteger(result[letters])) {
-        result[letters]++
-    } else result[letters] = 1;
-  };
-delete result[' '];
-if (Object.keys(result) === []) result = "undefined";
-return result;
+  const result = {};
+  for (const letter of myString) {
+    if (result[letter]) {
+      result[letter]++;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  delete result[' '];
+  return result;
 };
 
 const assertEqual = function(actual, expected) {
-  if (actual === expected ) {
+  if (actual === expected) {
     return console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } 
-  else return console.log(`Assertion Failed: ${actual} == ${expected}`);
+  } else {
+    return console.log(`Assertion Failed: ${actual} == ${expected}`);
+  }
 };
 
 const result1 = countLetters("lighthouse in the house");
